1f1519e4d22c1204cfc3fe29e147d7070aa02e78
xvoorvaa <xander@xvproductions.com>
2023-07-05 13:44:35 +0200

    Calls GSheet less to let it build

--- src/services/gsheet/index.ts
+++ src/services/gsheet/index.ts
@@ -10,9 +10,9 @@ import { Z_UNKNOWN } from 'zlib'
 
 /* -------------------- */
 
-const SPEAKER_DATA_RANGE = 'B3:F'
+const SPEAKER_DATA_RANGE = 'B3:G'
 
-const SESSION_DATA_RANGE = 'A3:K'
+const SESSION_DATA_RANGE = 'A3:L'
 
 /* -------------------- */
 
@@ -36,22 +36,6 @@ async function connectToGoogleSheets(config: DataConfig) {
   })
 
   return sheets
-}
-
-async function getSheetNames(config: DataConfig) {
-  const sheets = await connectToGoogleSheets(config)
-  const sheetId = process.env.NEXT_PUBLIC_DATA_CONFIG_SHEETID as string
-
-  const sheetsResponse = await API_QUEUE.add(() =>
-    sheets.spreadsheets.get({
-      spreadsheetId: sheetId,
-    })
-  )
-
-  const sheetNames = sheetsResponse?.data?.sheets?.map((i: any) => i.properties.title)
-  if (!sheetNames || sheetNames.length === 0) throw new Error('No valid sheet names found')
-
-  return sheetNames
 }
 
 async function getDataForRange(config: DataConfig, sheetName: string, range: string): Promise<any> {
@@ -87,109 +71,76 @@ export async function GetStages(config: DataConfig): Promise<Stage[]> {
 }
 
 export async function getSpeakers(config: DataConfig): Promise<Speaker[]> {
-  const sheetNames = await getSheetNames(config);
-  
   let allSpeakers: Speaker[] = [];
 
-  for (const sheetName of sheetNames) {
-    if (sheetName === 'Presentation Schedule - 15 July' || sheetName === 'Presentation Schedule - 16 July') { 
-      const data = await getDataForRange(config, sheetName, SPEAKER_DATA_RANGE);
-      
-      const speakerNames = [...new Set(data.flat().filter(Boolean))];
+  const data = await getDataForRange(config, 'Presentation Schedule', SPEAKER_DATA_RANGE);
 
-      const speakers = speakerNames.map(name => ({
-        id: name,
-        name,
-      }));
+  const speakerNames = [...new Set(data.flat().filter(Boolean))];
 
-      // @ts-ignore
-      allSpeakers.push(...speakers);
-    }
-  }
+  const speakers = speakerNames.map(name => ({
+    id: name,
+    name,
+  }));
+
+  // @ts-ignore
+  allSpeakers.push(...speakers);
 
   return allSpeakers;
 }
 
-export async function getSessions(config: DataConfig): Promise<Session[]> {
-  const sheetNames = await getSheetNames(config)
-  const speakerData = await getSpeakers(config)
-  const stageData = await GetStages(config)
+function getEndTime(time: string, duration: string) {
+  const [startHours, startMinutes] = time.split(':').map(Number);
+  const [durationHours, durationMinutes] = duration.split(':').map(Number);
 
-  const sessions: Session[] = []
+  let endHours = startHours + durationHours;
+  let endMinutes = startMinutes + durationMinutes;
 
-  for (const sheetName of sheetNames) {
-    if (sheetName.includes('Presentation Schedule')) {
-      const data = await getDataForRange(config, sheetName, SESSION_DATA_RANGE)
-
-      const dateString = sheetName.replace('Presentation Schedule - ', '')
-      const date = new Date(`2023 ${dateString}`)
-
-      const sessionData = data.map((row: any) => {
-        const [Time, Speaker1, Speaker2, Speaker3, Speaker4, Speaker5, Talk, EM, Duration, video] = row
-        if (GetSlug(Talk) === '') return null
-
-        const speakersRaw = [Speaker1, Speaker2, Speaker3, Speaker4, Speaker5].map((id: string) => {
-          let speaker
-
-          try {
-            if (!id) return null
-
-            speaker = speakerData.find((i) => {
-              return i.id === id
-            })
-            if (!speaker) throw new Error(`No speaker found for id ${id}`)
-          } catch (error) {
-            console.log(error)
-            return null
-          }
-
-          return speaker
-        })
-
-        const speakers = speakersRaw.filter((i) => i !== null)
-
-        let stage
-        try {
-          stage = stageData.find((i) => i.id === GetSlug('Main Stage'))
-          if (!stage) throw new Error(`No stage found for id Main Stage`)
-        } catch (error) {
-          console.error(error)
-          return null
-        }
-
-        let [TimeHours, TimeMinutes] = Time.split(':').map(Number)
-        let [DurationHours, DurationMinutes] = Duration.split(':').map(Number)
-
-        let startDate = new Date(date.getTime())
-        startDate.setHours(TimeHours, TimeMinutes)
-
-        let endDate = new Date(startDate.getTime())
-        endDate.setHours(endDate.getHours() + DurationHours)
-        endDate.setMinutes(endDate.getMinutes() + DurationMinutes)
-
-        let startStr = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)} ${Time}`
-        let endStr = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)} ${('0' + endDate.getHours()).slice(-2)}:${('0' + endDate.getMinutes()).slice(-2)}`
-
-        const start = datetimeToUnixTimestamp(startStr)
-        const end = datetimeToUnixTimestamp(endStr) 
-
-        return {
-          id: GetSlug(Talk),
-          name: Talk,
-          description: 'Placeholder...', //TODO: Lil description?
-          start,
-          end,
-          stage,
-          speakers,
-          video: video ?? null,
-        }
-      })
-
-      sessions.push(...sessionData.filter((i: any) => i !== null))
-    }
+  if (endMinutes >= 60) {
+    endHours += Math.floor(endMinutes / 60);
+    endMinutes = endMinutes % 60;
   }
 
-  return sessions
+  const endTime = `${String(endHours).padStart(2, '0')}:${String(endMinutes).padStart(2, '0')}`;
+
+  return endTime;
+}
+
+export async function getSessions(config: DataConfig): Promise<Session[]> {
+  const sheetName = 'Presentation Schedule';
+  const speakerData = await getSpeakers(config);
+  const stageData = await GetStages(config);
+
+  const sessions: Session[] = [];
+
+  const data = await getDataForRange(config, sheetName, SESSION_DATA_RANGE);
+
+  const sessionData = data.map((row: any) => {
+    const [Time, Date, Speaker1, Speaker2, Speaker3, Speaker4, Speaker5, Talk, EM, Duration, video] = row;
+    if (GetSlug(Talk) === '') return null;
+
+    const speakers = [Speaker1, Speaker2, Speaker3, Speaker4, Speaker5]
+      .map(id => id ? speakerData.find(i => i.id === id) || null : null)
+      .filter(Boolean);
+
+    const stage = stageData.find(i => i.id === GetSlug('Main Stage')) || null;
+
+    const start = datetimeToUnixTimestamp(`${Date} ${Time}`);
+    const endTime = getEndTime(Time, Duration);
+    const end = datetimeToUnixTimestamp(`${Date} ${endTime}`);
+
+    return {
+      id: GetSlug(Talk),
+      name: Talk,
+      description: 'Placeholder...',
+      start,
+      end,
+      stage,
+      speakers,
+      video: video ?? null,
+    };
+  });
+
+  return sessionData.filter(Boolean);
 }
 
 export async function GetSchedule(config: DataConfig): Promise<Session[]> {
